343f710358d44ed74491195240f5f92526edf75d
Rebel <allesrebel@yahoo.com>
2022-07-17 16:42:13 +0000

    Initial commit of implementation of COTP Missing integration of the 2FA from Tom's Codebase Missing client scripts to gen users .gitignore 	Ignores generated sqlite3 db nginx/* 	Added configs needed to extract out connection features server.py 	Example HTTP server showing features can be used

---
+++ server.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# A web server to echo back a request's headers and data.
+# TODO: Add in Argument Parser
+
+##
+# A local database to perserve registered users
+# Schemea is simply going to be
+# id:int (primary key), user_token:str, shared_secret:str
+#
+# To keep things as simple as possible, users will
+# be added manually via script. Then upon click from
+# the user, a shared secret will be made only if they
+# don't have one saved in the database.
+# This shared secret will be saved in their auth
+# application like anyother secret. 
+#   -> User goes to QR Code/Secret Entry Page
+# If user already has secret stored in DB, then 
+# the secret is used along with other factors to 
+# generate a connection-based one time passcode
+#   -> User goes to continue screen, with passcode presented
+##
+import sqlite3
+con = sqlite3.connect('website.db')
+
+# Check if we have a table
+table_check = "SELECT name FROM sqlite_master WHERE type='table' AND name='users';"
+if con.execute(table_check).rowcount == -1:
+    print('Creating Schema for database')
+    # Create the table, it doesn't exist
+    con.execute("""
+    create table users
+    (
+        id int not null,
+        user_token unqiue varchar(32) not null,
+        shared_secret varchar(32),
+        primary key (id)
+    );
+    """)
+
+##
+# The actual webserver part
+## 
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HTTPHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.write_response(b'')
+
+    def do_POST(self):
+        content_length = int(self.headers.get('content-length', 0))
+        body = self.rfile.read(content_length)
+
+        self.write_response(body)
+
+    def write_response(self, content):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(content)
+
+        print(self.headers)
+        print(content.decode('utf-8'))
+
+print('About to listen on http://localhost:8000')
+http_server = HTTPServer(('localhost', 8000), HTTPHandler)
+http_server.serve_forever()
